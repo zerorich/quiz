0b2b3de43668f00147700d68d5ed08d360b669f5
zerorich <167667718+zerorich@users.noreply.github.com>
2026-05-10 23:21:42 +0500

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.CALLBACK_URL || 'http://localhost:5000/auth/google/callback',
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
